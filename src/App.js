460208d64d8448a360990a86de11f275fb92ea92
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 11:45:16 +0300

    Added an import for Sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { CssBaseline } from "@mui/material";
 import { colorModeContext, useMode } from "./theme";
 import { ThemeProvider } from "@emotion/react";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
 
 
 function App()
