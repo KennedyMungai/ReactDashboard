b51c76bfd5d949e083e011cf02a6f0bb099f9ac7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 12:37:31 +0300

    Added styles to the Box component

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -26,7 +26,25 @@ const Sidebar = () =>
     const [selected, setSelected] = useState('Dashboard')
 
     return (
-        <Box>
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important"
+                },
+                "& .pro-menu-item:active": {
+                    color: "#6870fa !important"
+                }
+            }}
+        >
 
         </Box>
     )
