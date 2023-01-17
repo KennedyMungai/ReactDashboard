2838d2b6e3b9a1347e33ea005570dda7bcf7718f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 11:58:20 +0300

    Commented out the references and the logic for the component items

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { Route, Routes } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/dashboard/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar";
+// import Team from "./scenes/dashboard/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 
 function App()
@@ -29,7 +29,7 @@ function App()
           <Topbar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/team" element={<Team />} />
+            {/* <Route path="/team" element={<Team />} />
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/invoices" element={<Invoices />} />
             <Route path="/bar" element={<Bar />} />
@@ -38,7 +38,7 @@ function App()
             <Route path="/pie" element={<Pie />} />
             <Route path="/faq" element={<FAQ />} />
             <Route path="/geography" element={<Geography />} />
-            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/calendar" element={<Calendar />} /> */}
           </Routes>
         </main>
       </div>
