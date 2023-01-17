c213b0e8f4022738a71c405ef8c0a5454c781e26
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 12:28:05 +0300

    Set the selected state

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -23,6 +23,7 @@ const Sidebar = () =>
     const theme = useTheme()
     const colors = tokens(theme.palette.mode)
     const [isCollapsed, setIsCollapsed] = useState(false)
+    const [selected, setSelected] = useState('Dashboard')
 
     return (
         <div>
