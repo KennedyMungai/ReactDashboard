a7ea8fda1572ea2e03e5b1fa4ab844acb82eec49
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 10:44:16 +0300

    Imported tghe LightModeOutlined package

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,6 +1,7 @@
 import { Box, IconButton, InputBase, useTheme } from '@mui/material'
 import React, { useContext } from 'react'
 import { colorModeContext, tokens } from '../../theme'
+import { LightModeOutlined } from '@mui/icons-material'
 
 
 const Topbar = () =>
