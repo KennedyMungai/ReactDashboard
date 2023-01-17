99febec8f506092d92c2a5a515529858e541aaa1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 10:40:29 +0300

    Added an import for InputBase

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,6 @@
-import { Box, IconButton, useTheme } from '@mui/material'
+import { Box, IconButton, InputBase, useTheme } from '@mui/material'
 import React, { useContext } from 'react'
 import { colorModeContext, tokens } from '../../theme'
-
 
 
 const Topbar = () =>
