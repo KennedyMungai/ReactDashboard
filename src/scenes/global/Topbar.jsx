c947fb45a06f4c8bf97df91d9441e1e30260f62e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 10:59:49 +0300

    Added imports for icons

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,8 +1,12 @@
 import { Box, IconButton, InputBase, useTheme } from '@mui/material'
 import React, { useContext } from 'react'
 import { colorModeContext, tokens } from '../../theme'
-import { LightModeOutlined } from '@mui/icons-material'
-import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
+import LightModeOutlined from '@mui/icons-material/LightModeOutlined'
+import DarkModeOutlined from '@mui/icons-material/DarkModeOutlined'
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlined from '@mui/icons-material/PersonOutlined'
+import SearchIcon from '@mui/icons-material/Search';
 
 
 const Topbar = () =>
