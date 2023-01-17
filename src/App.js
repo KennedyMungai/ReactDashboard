4a284673ca2d477d3e89761c79f2b909f6aeaa18
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 11:59:11 +0300

    Commented out all the routes individually

--- src/App.js
+++ src/App.js
@@ -29,16 +29,16 @@ function App()
           <Topbar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            {/* <Route path="/team" element={<Team />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/invoices" element={<Invoices />} />
-            <Route path="/bar" element={<Bar />} />
-            <Route path="/line" element={<Line />} />
-            <Route path="/form" element={<Form />} />
-            <Route path="/pie" element={<Pie />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/geography" element={<Geography />} />
-            <Route path="/calendar" element={<Calendar />} /> */}
+            {/* <Route path="/team" element={<Team />} /> */}
+            {/* <Route path="/contacts" element={<Contacts />} /> */}
+            {/* <Route path="/invoices" element={<Invoices />} /> */}
+            {/* <Route path="/bar" element={<Bar />} /> */}
+            {/* <Route path="/line" element={<Line />} /> */}
+            {/* <Route path="/form" element={<Form />} /> */}
+            {/* <Route path="/pie" element={<Pie />} /> */}
+            {/* <Route path="/faq" element={<FAQ />} /> */}
+            {/* <Route path="/geography" element={<Geography />} /> */}
+            {/* <Route path="/calendar" element={<Calendar />} /> */}
           </Routes>
         </main>
       </div>
