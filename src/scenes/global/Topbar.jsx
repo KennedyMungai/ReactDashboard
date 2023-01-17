9640379a3f7dc8ea65288b9ab99f1cf71df201d1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 10:46:51 +0300

    Used proper icon imports

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -2,6 +2,7 @@ import { Box, IconButton, InputBase, useTheme } from '@mui/material'
 import React, { useContext } from 'react'
 import { colorModeContext, tokens } from '../../theme'
 import { LightModeOutlined } from '@mui/icons-material'
+import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
 
 
 const Topbar = () =>
