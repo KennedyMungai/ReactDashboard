417f1f0e63b18acc0717d3ed6234567a5fd0aa2f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 12:10:16 +0300

    Imported Link from react-router-dom

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -2,6 +2,7 @@ import { Box, IconButton, Typography, useTheme } from '@mui/material'
 import React, { useState } from 'react'
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar'
 import 'react-pro-sidebar/dist/css/styles.css'
+import { Link } from 'react-router-dom'
 
 
 const Sidebar = () =>
