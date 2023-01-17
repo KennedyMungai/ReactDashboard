fd1c3b24e0eb6d8e2a21512a3fed89b159b67406
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 11:50:03 +0300

    Added imports for the various components to be shown on the dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import { ThemeProvider } from "@emotion/react";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/dashboard/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
 
 
 function App()
