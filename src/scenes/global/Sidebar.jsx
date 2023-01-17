7f33af4c968aa1fa459ec0b74bc55877026124c5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 12:21:52 +0300

    Commented out the css file import line

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { Box, IconButton, Typography, useTheme } from '@mui/material'
 import React, { useState } from 'react'
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar'
-import 'react-pro-sidebar/dist/css/styles.css'
+// import 'react-pro-sidebar/dist/css/styles.css'
 import { Link } from 'react-router-dom'
 import { tokens } from '../../theme'
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
