bdb69282051bbea21650f993a4ba0d2a77a79cfa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 12:09:36 +0300

    Added some imports for material UI

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Box, IconButton, Typography, useTheme } from '@mui/material'
 import React, { useState } from 'react'
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar'
 import 'react-pro-sidebar/dist/css/styles.css'
