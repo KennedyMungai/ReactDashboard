083ae1cf85009a1dd122a72d1c754dc174f20d89
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 10:11:13 +0300

    Set up the objects for both the light mode and the dark mode

--- src/theme.js
+++ src/theme.js
@@ -126,4 +126,41 @@ const tokens = (mode) =>
 export const themeSettings = (mode) => 
 {
     const colors = tokens(mode)
+
+    return {
+        palette: {
+            mode: mode,
+            ...(mode === 'dark' ? {
+                primary: {
+                    main: colors.primary[500]
+                },
+                secondary: {
+                    main: colors.greenAccent[500]
+                },
+                neutral: {
+                    dark: colors.gray[700],
+                    main: colors.gray[500],
+                    light: colors.gray[500]
+                },
+                background: {
+                    default: colors.primary[500]
+                }
+            } : {
+                primary: {
+                    main: colors.primary[100]
+                },
+                secondary: {
+                    main: colors.greenAccent[500]
+                },
+                neutral: {
+                    dark: colors.gray[700],
+                    main: colors.gray[500],
+                    light: colors.gray[500]
+                },
+                background: {
+                    default: '#fcfcfc'
+                }
+            })
+        }
+    }
 }
