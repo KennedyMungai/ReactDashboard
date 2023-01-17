6b06232e36e266a2f368b8bf16d487109e96da80
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 12:11:02 +0300

    Imported tokens fron themes

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar'
 import 'react-pro-sidebar/dist/css/styles.css'
 import { Link } from 'react-router-dom'
+import { tokens } from '../../theme'
 
 
 const Sidebar = () =>
