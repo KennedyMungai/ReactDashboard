8fa1b1e2735184b2acfb38cb3433e034ffbea522
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 11:57:27 +0300

    Added a reference to the yet to be made Calendar component and implemented some routing logic for it

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
+import Calendar from "./scenes/calendar";
 
 
 function App()
@@ -37,6 +38,7 @@ function App()
             <Route path="/pie" element={<Pie />} />
             <Route path="/faq" element={<FAQ />} />
             <Route path="/geography" element={<Geography />} />
+            <Route path="/calendar" element={<Calendar />} />
           </Routes>
         </main>
       </div>
